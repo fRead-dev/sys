ee713325ba68572bda35ce1ecd85899166dd20d7
SunSung W541 <mail@embedded.biz.ua>
2024-07-24 21:48:10 +0200

    main [v0.0.2]  установка новой версии

--- const.go
+++ const.go
@@ -1,5 +1,12 @@
 package ParserInterface
 
-const GlobalVersion string = "1.5.9"
-const GlobalDateUpdate string = "05-27-2024"
-const GlobalName string = "ParserInterface"
+const (
+	GlobalName       string = "ParserInterface"
+	GlobalDateUpdate string = "07-24-2024"
+	GlobalHash       string = "298c89ff5f6e806fac5d994fcdfd6a76246d8c3c"
+
+	GlobalVersion      string = "v0.0.2"
+	GlobalVersionMajor string = "v0"
+	GlobalVersionMinor uint16 = 0
+	GlobalVersionPatch uint16 = 2
+)

befa1e1ef769878c3c37a1d2e590ae156edce5ae
SunSung W541 <mail@embedded.biz.ua>
2024-07-26 15:48:10 +0200

    main [v0.0.13]  master >> main

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package sys
 const (
 	GlobalName       string = "fReadSYS"
 	GlobalDateUpdate string = "07-26-2024"
-	GlobalHash       string = "03e3c104a6b280214727400925bcfebf8db90f3d"
+	GlobalHash       string = "cd89048ffd03260b78fb8f03059c22f8427c6017"
 
-	GlobalVersion      string = "v0.0.12"
+	GlobalVersion      string = "v0.0.13"
 	GlobalVersionMajor string = "v0"
 	GlobalVersionMinor uint16 = 0
-	GlobalVersionPatch uint16 = 12
+	GlobalVersionPatch uint16 = 13
 )

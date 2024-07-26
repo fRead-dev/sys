a9a4cb871aee1cbb553588bd3ae0986f7f60a8ec
SunSung W541 <mail@embedded.biz.ua>
2024-07-26 15:50:29 +0200

    main [v0.0.14]  fix codacy

--- const.go
+++ const.go
@@ -3,10 +3,10 @@ package sys
 const (
 	GlobalName       string = "fReadSYS"
 	GlobalDateUpdate string = "07-26-2024"
-	GlobalHash       string = "cd89048ffd03260b78fb8f03059c22f8427c6017"
+	GlobalHash       string = "befa1e1ef769878c3c37a1d2e590ae156edce5ae"
 
-	GlobalVersion      string = "v0.0.13"
+	GlobalVersion      string = "v0.0.14"
 	GlobalVersionMajor string = "v0"
 	GlobalVersionMinor uint16 = 0
-	GlobalVersionPatch uint16 = 13
+	GlobalVersionPatch uint16 = 14
 )
